d8cb9026979c2e63ae8e69a345c6834b9a01b27b
Robert Nations <briannations@Roberts-MacBook-Pro-2.local>
2016-08-16 12:40:11 -0700

    added lecture demo, lab req

---
+++ class-02-intro_js_html_css-arrays/lecture/app.js
@@ -0,0 +1,11 @@
+/* multi line comment */
+'use strict';
+
+// single line comment
+var kids = prompt('How many kids does Brian have?');
+console.log('number of kids: ', kids);
+
+// looking for lowercase name of brian
+if (kids === '3' || kids === '4') {
+  // do stuff here
+}
