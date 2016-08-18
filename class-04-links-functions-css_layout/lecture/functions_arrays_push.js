f7ee5789bef61dd1ff107685fc1c4361515505ff
Robert Nations <briannations@Roberts-MacBook-Pro-2.local>
2016-08-18 12:30:13 -0700

    added readme, demo, screenshot

---
+++ class-04-links-functions-css_layout/lecture/functions_arrays_push.js
@@ -0,0 +1,13 @@
+var arr = [];
+
+function makeArray() {
+    for (var i=0; i < 20; i++) {
+          arr.push(i);
+              console.log('iteration: ', arr);
+                }
+      console.log('final array: ', arr);
+}
+
+// call the function
+makeArray();
+
