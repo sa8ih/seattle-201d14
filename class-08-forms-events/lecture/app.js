36a410933d89d820d7b33237627bb19e0769aa45
Robert Nations <briannations@Roberts-MacBook-Pro-2.local>
2016-08-24 13:33:13 -0700

    added form, constructor, events demo

---
+++ class-08-forms-events/lecture/app.js
@@ -0,0 +1,40 @@
+var form = document.getElementById('sample_form');
+var table = document.getElementById('student_table');
+
+var data = [];
+
+function Student(first, last, status, plans) {
+  this.first = first;
+  this.last = last;
+  this.enrolled = status;
+  this.future_classes = plans;
+}
+
+function formData(event) {
+  event.preventDefault();
+
+  var first = event.target.first.value;
+  var last = event.target.last.value;
+  var enrolled = event.target.enrolled.checked;
+  var future_classes = event.target.future_classes.value;
+
+  data.push(new Student(first, last, enrolled, future_classes));
+
+  createTable();
+  form.reset();
+}
+
+function createTable() {
+  var row;
+  for (var i = 0; i < data.length; i++) {
+    row = document.createElement('tr');
+    row.innerHTML = '<td>' + data[i].first + '</td>' +
+      '<td>' + data[i].last + '</td>' +
+      '<td>' + data[i].enrolled + '</td>' +
+      '<td>' + data[i].future_classes + '</td>';
+  }
+
+  table.appendChild(row);
+}
+
+form.addEventListener('submit', formData);
